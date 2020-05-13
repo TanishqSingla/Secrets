617eefc18fce6ceca8f1bb160fbbf569ebbe47ae
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-13 18:22:43 +0530

    Setting up routes
    added route for home, login and register

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(3000, () => {
   console.log("Sarver is up at port 3000");
 });
