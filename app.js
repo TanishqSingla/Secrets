6e97cd69a3353e75eab35f532e837323852a1b61
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-13 18:48:09 +0530

    Adding mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Defining user schema
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -21,5 +35,5 @@ app.get("/register", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Sarver is up at port 3000");
+  console.log("Server is up at port 3000");
 });
